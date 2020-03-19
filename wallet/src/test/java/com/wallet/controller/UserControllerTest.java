986009f1cce1f79f9aa7a9ea5e41408f484f68ac
EduardoReis7 <eduardodovigireis@gmail.com>
2020-03-19 17:33:47 -0300

    Add SpringSecurity com criptografia de senhas && PostgreSQL com FlyWay

--- wallet/src/test/java/com/wallet/controller/UserControllerTest.java
+++ wallet/src/test/java/com/wallet/controller/UserControllerTest.java
@@ -46,12 +46,13 @@ public class UserControllerTest {
 
 		BDDMockito.given(service.save(Mockito.any(User.class))).willReturn(getMockUser());
 		
-		mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayLoad(ID, EMAIL, NAME, PASSWORD)).contentType(MediaType.APPLICATION_JSON)
+		mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayLoad(ID, EMAIL, NAME, PASSWORD))
+				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated())
 				.andExpect(jsonPath("$.data.id").value(ID))
 				.andExpect(jsonPath("$.data.email").value(EMAIL))
 				.andExpect(jsonPath("$.data.name").value(NAME))
-				.andExpect(jsonPath("$.data.password").value(PASSWORD));
+				.andExpect(jsonPath("$.data.password").doesNotExist());
 	}
 	
 	@Test
